7254c3282a4b921771d7d2725a75bdd050eb969a
Vidhi <vidhinainwal20@gmail.com>
2023-05-25 16:30:27 +0530

    sum

---
+++ test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("Enter two numbers:");
+    scanf("first number is:%d\nd=second number is:%d",&b,&b);
+    sum=a+b;
+    printf("Sum is:%d",sum);
+    return;
+}
